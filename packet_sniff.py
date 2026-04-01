da8928fbe872e51bac7fc1f848be7d0f0d8eabc5
adamoa3 <adamoa3@tcnj.edu>
2026-04-01 11:10:17 -0400

    Update packet_sniff.py
    Create packet sniffer worker class

--- packet_sniff.py
+++ packet_sniff.py
@@ -1,15 +1,36 @@
+from PyQt6.QtCore import QObject, pyqtSignal
 from scapy.all import sniff, IP, TCP, UDP
 
-# Processes packets, passed into sniff function
-def process_packet(pkt):
+class PacketSniff(QObject):
+    send_packet = pyqtSignal(dict)
 
+    def start(self):
+        sniff(prn=self.process_packet)
+
+    # Processes packets, passed into sniff function
+    def process_packet(self, pkt):
+        
+        # get values from packet
+        data = get_packet_values(pkt)
+        print(data)
+        print()
+
+        # apply rules from firewall.py
+
+        # send values + action to gui
+
+
+# gets packet values from a scapy packet
+def get_packet_values(pkt):
+    
     # dict that holds packet values to be later compared with firewall rules
+    # action has no value by default, once compared in firewall.py will be either "BLOCK" or "ALLOW"
     data = {"src_ip": None,
             "dst_ip": None,
             "src_port": None,
             "dst_port": None,
             "protocol": None,
-            "blocked": False}
+            "action": None}
 
     if pkt.haslayer(IP):
         data["src_ip"] = pkt[IP].src
@@ -28,8 +49,12 @@ def process_packet(pkt):
         data["protocol"] = "UDP" 
         #print(f"Source Port: {src_port}\nDestination Port: {dst_port}")
 
-    print(data)
-    print()
+    return data
+
 
 # Test
-packets = sniff(count=10, prn=process_packet)
+
+if __name__ == "__main__":
+    sniffer = PacketSniff()
+    sniffer.start()
+
