ac8353081478ff934ff5ae029534e5586fc0cc44
adamoa3 <adamoa3@tcnj.edu>
2026-04-07 14:45:15 -0400

    Refactor layout to use QVBox & QHBox instead of QGridLayout

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import sys
 
-from firewall import Firewall
+from firewall import Firewall, RuleWindow
 from packet_sniff import PacketSniffer
 
 from PyQt6.QtCore import QObject, QThread, pyqtSignal, QSize, Qt
@@ -8,7 +8,8 @@ from PyQt6.QtWidgets import (
     QApplication, 
     QWidget, 
     QMainWindow, 
-    QGridLayout,
+    QVBoxLayout,
+    QHBoxLayout,
     QLabel,
     QPushButton,
     QTableWidget,
@@ -28,49 +29,44 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Firewall Simulator")
         self.setGeometry(0, 0, 1000, 600)
 
-        # set up grid layout
+        # set up main layout
         center = QWidget()
+        layout = QVBoxLayout()
         self.setCentralWidget(center)
-        grid = QGridLayout()
-        center.setLayout(grid)
-
+        center.setLayout(layout)
 
         # start button
         self.start_button = QPushButton("Start")
         self.start_button.setFixedSize(80, 40)
         self.packet_sniffer.packet_signal.connect(self.handle_data)
         self.start_button.clicked.connect(self.packet_sniffer.start)
-        grid.addWidget(self.start_button, 0, 0, Qt.AlignmentFlag.AlignBottom | Qt.AlignmentFlag.AlignHCenter)
 
         # stop button
         self.stop_button = QPushButton("Stop")
         self.stop_button.setFixedSize(80, 40)
         self.stop_button.clicked.connect(self.packet_sniffer.stop)
-        grid.addWidget(self.stop_button, 0, 1, Qt.AlignmentFlag.AlignBottom | Qt.AlignmentFlag.AlignHCenter)
+
+        # set up button inner-layout
+        button_layout = QHBoxLayout()
+        button_layout.addWidget(self.start_button)
+        button_layout.addWidget(self.stop_button)
 
         # packet table
         self.pkt_table = QTableWidget()
         self.pkt_table.setColumnCount(6)
         self.pkt_table.setHorizontalHeaderLabels(["src_ip", "dst_ip", "protocol", "src_port", "dst_port", "action"])
-        grid.addWidget(self.pkt_table, 0, 0, Qt.AlignmentFlag.AlignVCenter)
+        
+        # format layout
+        layout.addWidget(self.pkt_table, Qt.AlignmentFlag.AlignHCenter)
+        layout.addLayout(button_layout)
 
 
 
     # takes in packet values and displays in GUI
     def handle_data(self, data):
-
-        ind = self.pkt_table.rowCount()
-        self.pkt_table.insertRow(ind)
-
-        self.pkt_table.setItem(ind, 0, QTableWidgetItem(data["src_ip"]))
-        self.pkt_table.setItem(ind, 1, QTableWidgetItem(data["dst_ip"]))
-        #self.pkt_table.setItem(ind, 2, QTableWidgetItem(data["protocol"]))
-        #self.pkt_table.setItem(ind, 3, QTableWidgetItem(data["src_port"]))
-        #self.pkt_table.setItem(ind, 4, QTableWidgetItem(data["dst_port"]))
-        self.pkt_table.setItem(ind, 5, QTableWidgetItem(data["action"]))
         
         # TESTING
-        #print(data)
+        print(data)
 
 
 
